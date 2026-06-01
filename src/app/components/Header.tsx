aea30e39f663d6bb17f6fc354067bb64deb64228
forrest <1165519525@qq.com>
2026-06-01 22:03:17 +0800

    🐞 fix: 修复 separate 组件不显示问题

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Separator } from "@/components/ui/separator"
 export default function Header() {
   return (
     <div className="h-16 px-10 border-b bg-white">
@@ -6,9 +7,11 @@ export default function Header() {
         <h1 className="text-2xl">
             <Link href="/">DUYI Store</Link>
         </h1>
-        <div className="flex justify-end space-x-4 text-sm">
+        <div className="flex justify-end space-x-4 text-sm h-1/3">
             <Link href="/search">Search</Link>
+            <Separator orientation="vertical" />
             <Link href="/account">Account</Link>
+            <Separator orientation="vertical" />
             <Link href="/cart">Cart</Link>
         </div>
       </div>
